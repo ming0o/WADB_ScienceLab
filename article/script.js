38925384a136203573cd5f8a28a4d8d4a5de9c8a
ming0o <https://github.com/ming0o>
2023-12-06 16:06:28 +0900

    ㅎㅇ

--- article/script.js
+++ article/script.js
@@ -15,13 +15,16 @@ allRects.forEach(function (rect) {
             originalZIndex = this.style.zIndex;
             this.style.zIndex = '4';
 
+            darkenLayer.style.display = 'block';
+            darkenLayer.style.zIndex = '3'; 
+            darkenLayer.style.opacity = '0.6';
+
             allRects.forEach(function (otherRect) {
                 if (otherRect !== rect) {
-                    otherRect.style.opacity = '0.3';
+                    otherRect.style.opacity = '1';
                 }
             });
 
-            darkenLayer.style.opacity = '0.8';
         } else {
             if (isEnlarged && this.style.zIndex === '4') {
                 this.style.transform = '';
@@ -32,9 +35,35 @@ allRects.forEach(function (rect) {
                     otherRect.style.opacity = '1';
                 });
 
-                darkenLayer.style.opacity = '0';
+                darkenLayer.style.display = 'none';
                 this.style.zIndex = originalZIndex;
             }
+        }
+
+        // Universe 또는 Earth를 클릭했을 때 modal_box1 스타일 적용
+        if (this.classList.contains('universe') || this.classList.contains('earth')) {
+            modal.querySelector('.modal_box').classList.remove('modal_box2');
+            modal.querySelector('.modal_box').classList.add('modal_box1');
+        }
+        // Moon 또는 Astronaut을 클릭했을 때 modal_box2 스타일 적용
+        else if (this.classList.contains('moon') || this.classList.contains('astronaut')) {
+            modal.querySelector('.modal_box').classList.remove('modal_box1');
+            modal.querySelector('.modal_box').classList.add('modal_box2');
+        }
+        
+        if (this.classList.contains('universe')) {
+            const uniTitle = this.querySelector('.uni_title');
+            if (uniTitle.classList.contains('uni_title1')) {
+                uniTitle.classList.remove('uni_title1');
+                uniTitle.classList.add('uni_title');
+            } else if (uniTitle.classList.contains('uni_title')) {
+                uniTitle.classList.remove('uni_title');
+                uniTitle.classList.add('uni_title1');
+            } 
+        } else if (this.classList.contains('earth')) {
+            const eaTitle = this.querySelector('.ea_title');
+            eaTitle.classList.remove('ea_title');
+            eaTitle.classList.add('ea_title1');
         }
     });
 });
@@ -43,7 +72,7 @@ const universe = document.querySelector('.universe');
 const rocket = document.getElementById('uni_rocket');
 let isFlying = false;
 
-universe.addEventListener('click', function() {
+universe.addEventListener('click', function () {
     const randomTop = Math.random() * 80; // 랜덤한 top 위치 설정
     const randomLeft = Math.random() * 80; // 랜덤한 left 위치 설정
 
@@ -60,7 +89,7 @@ universe.addEventListener('click', function() {
     }
 });
 
-rocket.addEventListener('transitionend', function() {
+rocket.addEventListener('transitionend', function () {
     // 트랜지션이 끝날 때 원래 위치로 이동
     if (!isFlying) {
         rocket.style.transition = ''; // 트랜지션 초기화
